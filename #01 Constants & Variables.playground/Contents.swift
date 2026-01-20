2852271f3dba2aef1dc684f17d7025d2a6fdebd4
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-01-20 14:07:56 +0300

    docs: add header comment to Constants & Variables playground

--- #01 Constants & Variables.playground/Contents.swift
+++ #01 Constants & Variables.playground/Contents.swift
@@ -1,3 +1,11 @@
+/*
+    Constants & Variables
+
+    Constants are values that cannot be changed once set, while variables can be modified after their initial assignment. In Swift, constants are declared using the `let` keyword, and variables are declared using the `var` keyword.
+
+    This example demonstrates how to declare constants and variables of different data types, including String and Int. It also shows how to change the value of a variable after its initial assignment.
+*/
+
 import Foundation
 
 let name: String = "Obada"
